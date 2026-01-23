8c13922f01d1770348ef184e87cc8f0344b00030
Isac Ernstsson <Isac Ernstson>
2026-01-23 20:59:58 +0100

    add debug print for invalid opcode

--- src/main.c
+++ src/main.c
@@ -51,6 +51,9 @@ int main(int argc, char* argv[])
     VmResult res = vm_run(&vm, &vm_program);
     if (res == VM_OK)
         printf("Program done\n");
+    else if (res == VM_ERR_INVALID_OPCODE)
+    printf("Wrong opcoden\n");
+        return 1;
 
     return 0;
 }
